0bc7a70e8f9221d799a320e5d6689a531eb39ba6
Jefferson Peña <jefferson152530@gmail.com>
2025-11-01 00:21:08 -0500

    zzz

---
+++ src/test/java/com/ejemplo/ejemplo/EjemploApplicationTests copy.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class VulnerableDeserializer {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fileIn = new FileInputStream("payload.ser");
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        Object obj = in.readObject(); // ⚠️ Peligroso si no se valida el tipo ni el origen
+        in.close();
+        fileIn.close();
+
+        System.out.println("Objeto deserializado: " + obj);
+    }
+}
