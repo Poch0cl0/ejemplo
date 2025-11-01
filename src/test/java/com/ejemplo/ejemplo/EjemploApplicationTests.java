55c9f8be4ceb31f01f05a212fbb2382bf416fba0
Jefferson Peña <jefferson152530@gmail.com>
2025-11-01 11:34:20 -0500

    agregando vulnearabilidad

--- src/test/java/com/ejemplo/ejemplo/EjemploApplicationTests.java
+++ src/test/java/com/ejemplo/ejemplo/EjemploApplicationTests.java
@@ -4,6 +4,7 @@ public class VulnerableDeserializer {
     public static void main(String[] args) throws Exception {
         FileInputStream fileIn = new FileInputStream("payload.ser");
         ObjectInputStream in = new ObjectInputStream(fileIn);
+        Object obj = in.readObject();
         Object obj = in.readObject(); // ⚠️ Peligroso si no se valida el tipo ni el origen
         in.close();
         fileIn.close();
